60b6dd39d3752804de9d7371e53e48d60f9d6981
wang_j2 <jun.wang@preh.cn>
2021-01-12 11:56:44 +0800

    新增cash自动上车

--- .github/jd_sharecodes.sh
+++ .github/jd_sharecodes.sh
@@ -7,8 +7,9 @@ PLANT_BEAN_SHARECODES_ARR=($PLANT_BEAN_SHARECODES)
 JXFACTORY_SHARECODES_ARR=($JXFACTORY_SHARECODES)
 DDFACTORY_SHARECODES_ARR=($DDFACTORY_SHARECODES)
 DREAM_FACTORY_SHARE_CODES_ARR=($DREAM_FACTORY_SHARE_CODES)
-JDZZ_INVITECODES_ARR=($JDZZ_INVITECODES)
+JDZZ_SHARECODES_ARR=($JDZZ_SHARECODES)
 JDJOY_SHARECODES_ARR=($JDJOY_SHARECODES)
+JD_CASH_SHARECODES _ARR=($JD_CASH_SHARECODES )
 
 for sharecode in ${FRUITSHARECODES_ARR[@]}
 do 
@@ -40,7 +41,7 @@ do
 curl 'http://api.turinglabs.net/api/v1/jd/ddfactory/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
 done
 
-for sharecode in ${JDZZ_INVITECODES_ARR[@]}
+for sharecode in ${JDZZ_SHARECODES_ARR[@]}
 do 
 curl 'https://code.chiang.fun/api/v1/jd/jdzz/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
 done
@@ -48,4 +49,8 @@ done
 for sharecode in ${JDJOY_SHARECODES_ARR[@]}
 do 
 curl 'https://code.chiang.fun/api/v1/jd/jdcrazyjoy/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
+
+for sharecode in ${JD_CASH_SHARECODES _ARR[@]}
+do 
+curl 'https://code.chiang.fun/api/v1/jd/jdcash/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
 done
