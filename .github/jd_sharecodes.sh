183c3c9a8b05ef887750703d6a8d3f0f7162beb4
nbzongzong <73403523+nbzongzong@users.noreply.github.com>
2021-01-17 17:22:55 +0800

    fix sharecode

--- .github/jd_sharecodes.sh
+++ .github/jd_sharecodes.sh
@@ -8,7 +8,7 @@ DDFACTORY_SHARECODES_ARR=($DDFACTORY_SHARECODES)
 DREAM_FACTORY_SHARE_CODES_ARR=($DREAM_FACTORY_SHARE_CODES)
 JDZZ_SHARECODES_ARR=($JDZZ_SHARECODES)
 JDJOY_SHARECODES_ARR=($JDJOY_SHARECODES)
-JD_CASH_SHARECODES _ARR=($JD_CASH_SHARECODES )
+#JD_CASH_SHARECODES _ARR=($JD_CASH_SHARECODES )
 
 for sharecode in ${FRUITSHARECODES_ARR[@]}
 do 
@@ -44,7 +44,7 @@ for sharecode in ${JDJOY_SHARECODES_ARR[@]}
 do 
 curl 'https://code.chiang.fun/api/v1/jd/jdcrazyjoy/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
 
-for sharecode in ${JD_CASH_SHARECODES _ARR[@]}
-do 
-curl 'https://code.chiang.fun/api/v1/jd/jdcash/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
-done
+# for sharecode in ${JD_CASH_SHARECODES _ARR[@]}
+# do 
+# curl 'https://code.chiang.fun/api/v1/jd/jdcash/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
+# done
