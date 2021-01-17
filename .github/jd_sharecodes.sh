7e41e5e4d5a3a244a973d32588f6cc3916c55ed0
nbzongzong <73403523+nbzongzong@users.noreply.github.com>
2021-01-17 17:24:59 +0800

    fix

--- .github/jd_sharecodes.sh
+++ .github/jd_sharecodes.sh
@@ -43,6 +43,7 @@ done
 for sharecode in ${JDJOY_SHARECODES_ARR[@]}
 do 
 curl 'https://code.chiang.fun/api/v1/jd/jdcrazyjoy/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
+done
 
 # for sharecode in ${JD_CASH_SHARECODES _ARR[@]}
 # do 
